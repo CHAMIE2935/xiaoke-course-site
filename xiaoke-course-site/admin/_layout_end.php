5d6bcf4d607759881b6a57e3cdc69e57ecd31d69
CHAMIE <123226144+CHAMIE2935@users.noreply.github.com>
2026-09-06 11:38:45 +0800

    Delete xiaoke-course-site directory

--- xiaoke-course-site/admin/_layout_end.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** 后台布局结尾 */
-$user = current_user();
-echo '</div></div>'; // .admin-main .admin-layout
-echo '<div style="text-align:center;padding:16px;font-size:12px;color:#b6bec9">'
-   . e($GLOBALS['config']['site']['name']) . ' 后台 · '
-   . '<a href="' . url('index.php') . '" style="color:#7a8694">前台首页</a> · '
-   . '<a href="' . url('logout.php') . '" style="color:#7a8694">退出登录（' . e($user['nickname'] ?: $user['email']) . '）</a>'
-   . '</div></body></html>';
--- xiaoke-course-site/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/includes/init.php';
-session_destroy();
-redirect(url('index.php'));
